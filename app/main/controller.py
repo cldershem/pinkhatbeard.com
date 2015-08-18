dcc3cfc64976851e8e7c26451ff662a81ee3095c
Cameron Dershem <cldershem@gmail.com>
2015-08-18 01:08:35 -0400

    Simplifies adding new contacts

--- app/main/controller.py
+++ app/main/controller.py
@@ -13,9 +13,33 @@ Main routes for application.
 from flask import render_template
 from . import main
 import random
+from collections import namedtuple
+
+
+def get_contacts():
+    Contact = namedtuple('Contact', 'label, display, link')
+    list_of_contacts = [
+        Contact('email', 'cldershem [at] gmail.com', None),
+        Contact('phone', '+1.312.361.0322', 'tel:+013123610322'),
+        Contact('github', 'github.com/cldershem',
+                'http://github.com/cldershem'),
+        Contact('twitter', '@jerknextdoor',
+                'https://twitter.com/jerknextdoor'),
+        Contact('resume', 'github.com/cldershem/resume',
+                'https://github.com/cldershem/Resume/' +
+                'blob/master/built/CameronDershemResume-Skills.pdf'),
+        Contact('freenode', 'jerknextdoor', None),
+        Contact('skype', 'cldershem', 'callto://cldershem'),
+        Contact('google+', '+CameronDershem',
+                'http://google.com/+CameronDershem'),
+        Contact('last.fm', 'cldershem', 'http://last.fm/user/cldershem'),
+        ]
+
+    return list_of_contacts
 
 
 @main.route('/')
+@main.route('/index')
 def index():
     list_o_adjs = [
         "loud",
@@ -26,10 +50,13 @@ def index():
         "hair farmer",
         "carries a purse",
         ]
+
     adj1 = 'pink hat'
     adj2 = 'beard'
     adj3 = random.choice(list_o_adjs)
+
     return render_template('index.html',
                            adj1=adj1,
                            adj2=adj2,
-                           adj3=adj3)
+                           adj3=adj3,
+                           contacts=get_contacts())
