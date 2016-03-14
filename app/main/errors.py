881f9a03958d538aa414bf051086578238ae8aac
Cameron Dershem <cldershem@gmail.com>
2016-03-14 01:37:10 -0400

    fixes type in main.errors

--- app/main/errors.py
+++ app/main/errors.py
@@ -24,7 +24,7 @@ def page_not_found(error):
 
 @main.app_errorhandler(500)
 def internal_server_error(error):
-    return render_template('errors/500`.html',
+    return render_template('errors/500.html',
                            adj1='human',
                            adj2='talented',
                            adj3='might make mistakes')
